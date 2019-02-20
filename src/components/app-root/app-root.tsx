d2e20c6e62744e73e37d6ddbedbc0f08e4be02b1
Stefan Kandlbinder <kans@bah.local>
2019-02-20 15:50:57 +0100

    first

---
+++ src/components/app-root/app-root.tsx
@@ -0,0 +1,24 @@
+import { Component } from '@stencil/core';
+
+
+@Component({
+  tag: 'app-root',
+  styleUrl: 'app-root.css',
+  shadow: true
+})
+export class AppRoot {
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Stencil App Starter</h1>
+        </header>
+
+        <main>
+         
+        </main>
+      </div>
+    );
+  }
+}
---
+++ src/components/lufti-main/lufti-main.tsx
@@ -0,0 +1,30 @@
+import { Component } from '@stencil/core';
+
+
+@Component({
+  tag: 'lufti-main',
+  styleUrl: 'lufti-main.css',
+  shadow: true
+})
+export class AppRoot {
+
+  render() {
+    return (
+      <div class="lufti-container">
+        <header class="lufti-header">
+          <h1 class="lufti-header__title">LUFTI</h1>
+        </header>
+
+        <main class="lufti-main">
+          <div class="lufti-air-component">
+            <svg class="lufti-air-component-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="airSVGLuftdatenMarker"><path d="M21.5 17.5v-11L12 1 2.5 6.5v11L12 23z"></path></svg>
+            <div class="lufti-air-component-value-container">
+              <div class="lufti-air-component-value">02,00</div>
+            </div>
+          </div>
+          <input class="lufti-search" placeholder="put in your sensor id"/>
+        </main>
+      </div>
+    );
+  }
+}
