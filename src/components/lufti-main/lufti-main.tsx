434fb57c1f2c79905a498b1bbedbf6abad06d6fe
Stefan Kandlbinder <kans@bah.local>
2019-02-21 14:12:43 +0100

    first prototype

--- src/components/lufti-main/lufti-main.tsx
+++ src/components/lufti-main/lufti-main.tsx
@@ -24,9 +24,11 @@ export class AppRoot {
 
   render() {
     let pm10 = "0.00";
+    let pm25 = "0.00";
 
     if (this.componentValues) {
       pm10 = this.componentValues[0].value;
+      pm25 = this.componentValues[1].value;
     }
 
     return (
@@ -39,7 +41,8 @@ export class AppRoot {
           <div class="lufti-air-component">
             <svg class="lufti-air-component-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="airSVGLuftdatenMarker"><path d="M21.5 17.5v-11L12 1 2.5 6.5v11L12 23z"></path></svg>
             <div class="lufti-air-component-value-container">
-              <div class="lufti-air-component-value">{pm10}</div>
+              <div class="lufti-air-component-value lufti-air-component-value--pm10">{pm10}</div>
+              <div class="lufti-air-component-value lufti-air-component-value--pm25">{pm25}</div>
             </div>
           </div>
           <lufti-search></lufti-search>
