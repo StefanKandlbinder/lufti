b4acdd3ef025395c36d33494fa7c9f072381817c
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2019-02-24 15:13:24 +0100

    refactoring

--- src/components/lufti-loading/lufti-loading.tsx
+++ src/components/lufti-loading/lufti-loading.tsx
@@ -19,7 +19,7 @@ export class LuftiLoading {
   render() {
     return (
       <svg class="lufti-loading-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path class="lufti-loading-icon-path" d="M3 18.5h18.7c.8-.7 1.3-1.7 1.3-2.9 0-1.7-1.1-3.1-2.7-3.6-.7-3.8-4-6.6-7.9-6.6-3.2 0-5.9 1.8-7.2 4.5-2.4.4-4.2 2.4-4.2 4.8 0 1.6.8 3 2 3.8z" fill="none" stroke="#000" stroke-miterlimit="10" />
+        <path d="M18.6 18.3c1.9 0 3.4-1.5 3.4-3.4 0-1.5-1-2.8-2.4-3.3-.6-3.4-3.6-6-7.2-6-2.9.1-5.4 1.7-6.6 4.1C3.7 10 2 11.8 2 14c0 2.4 1.9 4.3 4.3 4.3h12.3z" fill="none" stroke="#000" stroke-width=".8333" stroke-miterlimit="10"/>
       </svg>
     );
   }
