dd859b3ec16161861ffaa127f9363d2ca7ce382a
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2019-02-24 15:42:56 +0100

    refactoring

--- src/components/lufti-search/lufti-search.tsx
+++ src/components/lufti-search/lufti-search.tsx
@@ -67,7 +67,7 @@ export class LuftiSearch {
       .then(parsedRes => {
         this.luftiIDSelected.emit({ "pm10": parsedRes[parsedRes.length - 1].sensordatavalues[0].value,
           "pm25": parsedRes[parsedRes.length - 1].sensordatavalues[1].value,
-          "timestamp": parsedRes[1].timestamp });
+          "timestamp": parsedRes[parsedRes.length - 1].timestamp });
         this.loading = false;
         this.isLoading.emit(false);
         // this.error = '';
