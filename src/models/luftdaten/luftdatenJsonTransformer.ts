1c66d24d58c6ede4fcf590443cf916a5bf547c1c
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2019-03-09 10:47:49 +0100

    refactoring

--- src/models/luftdaten/luftdatenJsonTransformer.ts
+++ src/models/luftdaten/luftdatenJsonTransformer.ts
@@ -2,17 +2,24 @@ import getStringDateLuftdaten from '../../utilities/getStringDateLuftdaten';
 import { Luftdaten } from './luftdaten';
 
 export default function luftdatenJsonTransformer(response) {
+  let pm10 = "0";
+  let pm25 = "0";
+  let longitude = "";
+  let latitude = "";
+  let timestamp = "";
 
-  const pm10 = response[response.length - 1].sensordatavalues[0].value_type === "P1" ?
-    response[response.length - 1].sensordatavalues[0].value : "0";
-  const pm25 = response[response.length - 1].sensordatavalues[1].value_type === "P2" ?
-    response[response.length - 1].sensordatavalues[1].value : "0";;
-  const timestamp = response[response.length - 1].sensordatavalues[0].value_type === "P1" ?
-    getStringDateLuftdaten(response[response.length - 1].timestamp) : "";
-  const latitude = response[response.length - 1].sensordatavalues[0].value_type === "P1" ?
-    response[response.length - 1].location.latitude : "";
-  const longitude = response[response.length - 1].sensordatavalues[0].value_type === "P1" ?
-    response[response.length - 1].location.longitude : "";
+  if (response.length) {
+    pm10 = response[response.length - 1].sensordatavalues[0].value_type === "P1" ?
+      response[response.length - 1].sensordatavalues[0].value : "0";
+    pm25 = response[response.length - 1].sensordatavalues[1].value_type === "P2" ?
+      response[response.length - 1].sensordatavalues[1].value : "0";;
+    timestamp = response[response.length - 1].sensordatavalues[0].value_type === "P1" ?
+      getStringDateLuftdaten(response[response.length - 1].timestamp) : "";
+    latitude = response[response.length - 1].sensordatavalues[0].value_type === "P1" ?
+      response[response.length - 1].location.latitude : "";
+    longitude = response[response.length - 1].sensordatavalues[0].value_type === "P1" ?
+      response[response.length - 1].location.longitude : "";
+  }
 
   const components = { pm10, pm25 };
   const location = { longitude, latitude };
