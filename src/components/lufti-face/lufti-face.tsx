3cb6bc0d5edef1b02198e94d80ebf5e2c4a50706
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2019-11-10 15:35:33 +0100

    face

--- src/components/lufti-face/lufti-face.tsx
+++ src/components/lufti-face/lufti-face.tsx
@@ -10,10 +10,12 @@ export class LuftiFace {
   raf = null;
   duration: number = 500;
 
-  @State() mouth: number = 270;
+  @State() mouth: number = 290;
   @State() eyeLeft: number = 30;
   @State() eyeRight: number = 30;
-  @State() eyeBallLeft: number = 85;
+  @State() eyeBallLeft: number = 105;
+  @State() eyeBrowLeft: number = 0;
+  @State() eyeBrowRight: number = 0;
 
   @Prop({ mutable: true, reflect:true }) mood: number = 0;
   @Watch('mood')
@@ -41,6 +43,8 @@ export class LuftiFace {
         this.animateMouth(progress, from, to)
         this.animateEyeLeft(progress, from, to)
         this.animateEyeBallLeft(progress, from, to)
+        this.animateEyeBrowLeft(progress, from, to)
+        this.animateEyeBrowRight(progress, from, to)
 			}
 			else {
 				window.cancelAnimationFrame(this.raf);
@@ -51,8 +55,8 @@ export class LuftiFace {
   }
 
   animateMouth(progress, from, to) {
-    const _from = (from > 100) ? 170 : (220 - from) + 50;
-    const _to = (to > 100) ? 170 : (220 - to) + 50;
+    const _from = (from > 50) ? 190 : this.mapTo(from, 0, 50, 260, 190);
+    const _to = (to > 50) ? 190 : this.mapTo(to, 0, 50, 260, 190);
     const goTo = Math.abs(_from - _to);
 
     if (_to > _from && this.mouth) {
@@ -77,8 +81,8 @@ export class LuftiFace {
   }
 
   animateEyeBallLeft(progress, from, to) {
-    const _from = (from > 100) ? 100 : this.mapTo(from, 0, 100, 85, 100);
-    const _to = (to > 100) ? 100 : this.mapTo(to, 0, 100, 85, 100);
+    const _from = (from > 100) ? 120 : this.mapTo(from, 0, 100, 105, 120);
+    const _to = (to > 100) ? 120 : this.mapTo(to, 0, 100, 105, 120);
     const goTo = Math.abs(_from - _to);
 
     if (_to > _from && this.eyeLeft) {
@@ -89,23 +93,53 @@ export class LuftiFace {
     }
   }
 
+  animateEyeBrowLeft(progress, from, to) {
+    const _from = (from > 100) ? -20 : this.mapTo(from, 0, 100, 10, -20);
+    const _to = (to > 100) ? -20 : this.mapTo(to, 0, 100, 10, -20);
+    const goTo = Math.abs(_from - _to);
+
+    if (_to > _from && this.eyeBrowLeft) {
+      this.eyeBrowLeft = Math.round((progress * goTo) + _from);
+    }
+    else {
+      this.eyeBrowLeft = _from - Math.round((progress * goTo));
+    }
+  }
+
+  animateEyeBrowRight(progress, from, to) {
+    const _from = (from > 100) ? 40 : this.mapTo(from, 0, 100, 20, 40);
+    const _to = (to > 100) ? 40 : this.mapTo(to, 0, 100, 20, 40);
+    const goTo = Math.abs(_from - _to);
+
+    if (_to > _from && this.eyeBrowLeft) {
+      this.eyeBrowRight = Math.round((progress * goTo) + _from);
+    }
+    else {
+      this.eyeBrowRight = _from - Math.round((progress * goTo));
+    }
+  }
+
   mapTo(value, in_min, in_max, out_min, out_max) {
     return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
   }
 
   render() {
-    let mouth = `M40 180 Q70 220, 140 ${this.mouth} T 260 220`
+    let mouth = `M40 200 Q70 240, 140 ${this.mouth} T 260 240`;
+    let eyeBrowLeft = `M50 20 Q 95 ${this.eyeBrowLeft} 130 20`;
+    let eyeBrowRight = `M150 20 Q 195 ${this.eyeBrowRight} 230 20`;
     let eyeLeft = <g>
-        <ellipse cx="90" cy="65" rx="30" ry={this.eyeLeft} fill="rgba(255, 255, 255, 0.9)" stroke="white"/>
+        <path id="lufti-face__eye-brow-left" d={eyeBrowLeft} stroke="white" fill="transparent"/>
+        <ellipse cx="90" cy="85" rx="30" ry={this.eyeLeft} fill="rgba(255, 255, 255, 0.9)" stroke="white"/>
         <circle cx="90" cy={this.eyeBallLeft} r="10" fill="rgba(0, 0, 0, 0.9)" stroke="white"></circle>
       </g>
     let eyeRight = <g>
-        <ellipse cx="190" cy="65" rx="30" ry={this.eyeRight} fill="rgba(255, 255, 255, 0.9)" stroke="white"/>
-        <circle cx="190" cy="85" r="10" fill="rgba(0, 0, 0, 0.9)" stroke="white"></circle>
+        <path id="lufti-face__eye-brow-right" d={eyeBrowRight} stroke="white" fill="transparent"/>
+        <ellipse cx="190" cy="85" rx="30" ry={this.eyeRight} fill="rgba(255, 255, 255, 0.9)" stroke="white"/>
+        <circle cx="190" cy="105" r="10" fill="rgba(0, 0, 0, 0.9)" stroke="white"></circle>
       </g>
 
     return (
-      <svg id="lufti-face" viewBox="0 0 300 300">
+      <svg id="lufti-face" viewBox="0 -10 300 300">
         <path id="lufti-face__mouth" d={mouth} stroke="white" fill="transparent"/>
         {eyeLeft}
         {eyeRight}
