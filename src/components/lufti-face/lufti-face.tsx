0befb51677b2a8923d45cc7f2f5a073a7415552a
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2019-11-10 14:06:20 +0100

    face

--- src/components/lufti-face/lufti-face.tsx
+++ src/components/lufti-face/lufti-face.tsx
@@ -10,7 +10,11 @@ export class LuftiFace {
   raf = null;
   duration: number = 500;
 
-  @State() mouth: number = 150;
+  @State() mouth: number = 270;
+  @State() eyeLeft: number = 30;
+  @State() eyeRight: number = 30;
+  @State() eyeBallLeft: number = 85;
+
   @Prop({ mutable: true, reflect:true }) mood: number = 0;
   @Watch('mood')
   watchHandler(newValue: number, oldValue: number) {
@@ -25,12 +29,8 @@ export class LuftiFace {
 		const time = {
 			start: performance.now(),
       total: this.duration,
-      elapsed: 0,
-      from: (from > 100) ? 50 : (100 - from) + 50,
-      to: (to > 100) ? 50 : (100 - to) + 50,
+      elapsed: 0
     }
-
-    const goTo = Math.abs(time.from - time.to);
 
 		let step = now => {
 			time.elapsed = now - time.start;
@@ -38,14 +38,9 @@ export class LuftiFace {
 
 			if (progress <= 1.1) {
         this.raf = window.requestAnimationFrame(step);
-        if (time.to > time.from && this.mouth) {
-          this.mouth = Math.round((progress * goTo) + time.from);
-        }
-        else {
-          this.mouth = time.from - Math.round((progress * goTo));
-        }
-
-        console.info(time.from, time.to, goTo, this.mouth);
+        this.animateMouth(progress, from, to)
+        this.animateEyeLeft(progress, from, to)
+        this.animateEyeBallLeft(progress, from, to)
 			}
 			else {
 				window.cancelAnimationFrame(this.raf);
@@ -55,12 +50,65 @@ export class LuftiFace {
 		this.raf = window.requestAnimationFrame(step);
   }
 
+  animateMouth(progress, from, to) {
+    const _from = (from > 100) ? 170 : (220 - from) + 50;
+    const _to = (to > 100) ? 170 : (220 - to) + 50;
+    const goTo = Math.abs(_from - _to);
+
+    if (_to > _from && this.mouth) {
+      this.mouth = Math.round((progress * goTo) + _from);
+    }
+    else {
+      this.mouth = _from - Math.round((progress * goTo));
+    }
+  }
+
+  animateEyeLeft(progress, from, to) {
+    const _from = (from > 100) ? 60 : this.mapTo(from, 0, 100, 30, 60);
+    const _to = (to > 100) ? 60 : this.mapTo(to, 0, 100, 30, 60);
+    const goTo = Math.abs(_from - _to);
+
+    if (_to > _from && this.eyeLeft) {
+      this.eyeLeft = Math.round((progress * goTo) + _from);
+    }
+    else {
+      this.eyeLeft = _from - Math.round((progress * goTo));
+    }
+  }
+
+  animateEyeBallLeft(progress, from, to) {
+    const _from = (from > 100) ? 100 : this.mapTo(from, 0, 100, 85, 100);
+    const _to = (to > 100) ? 100 : this.mapTo(to, 0, 100, 85, 100);
+    const goTo = Math.abs(_from - _to);
+
+    if (_to > _from && this.eyeLeft) {
+      this.eyeBallLeft = Math.round((progress * goTo) + _from);
+    }
+    else {
+      this.eyeBallLeft = _from - Math.round((progress * goTo));
+    }
+  }
+
+  mapTo(value, in_min, in_max, out_min, out_max) {
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  }
+
   render() {
-    let mouth = `M 40 80 Q 70 120, 150 ${this.mouth} T 260 120`
+    let mouth = `M40 180 Q70 220, 140 ${this.mouth} T 260 220`
+    let eyeLeft = <g>
+        <ellipse cx="90" cy="65" rx="30" ry={this.eyeLeft} fill="rgba(255, 255, 255, 0.9)" stroke="white"/>
+        <circle cx="90" cy={this.eyeBallLeft} r="10" fill="rgba(0, 0, 0, 0.9)" stroke="white"></circle>
+      </g>
+    let eyeRight = <g>
+        <ellipse cx="190" cy="65" rx="30" ry={this.eyeRight} fill="rgba(255, 255, 255, 0.9)" stroke="white"/>
+        <circle cx="190" cy="85" r="10" fill="rgba(0, 0, 0, 0.9)" stroke="white"></circle>
+      </g>
 
     return (
-      <svg id="lufti-face" viewBox="0 0 300 200">
+      <svg id="lufti-face" viewBox="0 0 300 300">
         <path id="lufti-face__mouth" d={mouth} stroke="white" fill="transparent"/>
+        {eyeLeft}
+        {eyeRight}
       </svg>
     )
   }
