f874423432e28c67d8427116990e905474982ddc
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2019-11-16 22:13:17 +0100

    refactoring

--- src/sw.js
+++ src/sw.js
@@ -7,6 +7,19 @@ self.workbox.precaching.precacheAndRoute([]);
 }) */
 
 self.workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
+self.workbox.routing.registerRoute(
   new RegExp('https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/.*', 'g'),
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: 'api-cache',
