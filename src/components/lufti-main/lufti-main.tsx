586d04672814b101481a538a474e80989ffeb9da
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2019-11-16 13:05:03 +0100

    refactoring

--- src/components/lufti-main/lufti-main.tsx
+++ src/components/lufti-main/lufti-main.tsx
@@ -88,17 +88,22 @@ export class LuftiMain {
 
     return (
       <div class={luftiContainerClass}>
-        <header class="lufti-header">
+        <header class={this.isLoading ? "lufti-header s-loading" : "lufti-header"} >
           <h1 class="lufti-header__title">LUFTI
-          <svg class="lufti-header-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M18.6 18.3c1.9 0 3.4-1.5 3.4-3.4 0-1.5-1-2.8-2.4-3.3-.6-3.4-3.6-6-7.2-6-2.9.1-5.4 1.7-6.6 4.1C3.7 10 2 11.8 2 14c0 2.4 1.9 4.3 4.3 4.3h12.3z" fill="none" stroke="#000" stroke-width=".8333" stroke-miterlimit="10"/>
-          </svg>
+          <div class="lufti-header-logo-container">
+            <svg class="lufti-header-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path class="lufti-header-logo__path" d="M18.6 18.3c1.9 0 3.4-1.5 3.4-3.4 0-1.5-1-2.8-2.4-3.3-.6-3.4-3.6-6-7.2-6-2.9.1-5.4 1.7-6.6 4.1C3.7 10 2 11.8 2 14c0 2.4 1.9 4.3 4.3 4.3h12.3z" fill="none" stroke="#000" stroke-width=".8333" stroke-miterlimit="10"/>
+            </svg>
+            <svg class="lufti-header-logo-animated" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path class="lufti-header-logo__path-animated" d="M18.6 18.3c1.9 0 3.4-1.5 3.4-3.4 0-1.5-1-2.8-2.4-3.3-.6-3.4-3.6-6-7.2-6-2.9.1-5.4 1.7-6.6 4.1C3.7 10 2 11.8 2 14c0 2.4 1.9 4.3 4.3 4.3h12.3z" fill="none" stroke="#000" stroke-width=".8333" stroke-miterlimit="10"/>
+            </svg>
+          </div>
           </h1>
           <lufti-legend></lufti-legend>
         </header>
 
         <main class="lufti-main">
-          <div class={this.isLoading ? "lufti-air-component s-loading" : "lufti-air-component"}>
+          <div class="lufti-air-component">
             {luftiValues}
           </div>
           <lufti-face id="hankMoody" mood={parseFloat(this.luftdaten.components.pm10)}></lufti-face>
@@ -106,7 +111,6 @@ export class LuftiMain {
             <lufti-search></lufti-search>
             {luftiLocation}
           </div>
-          <lufti-loading class={ this.isLoading ? "s-loading" : "" }></lufti-loading>
           <a href="https://github.com/StefanKandlbinder/lufti" class="lufti-github" title="Visit Lufti on Github!">
             <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 48 48">
               <path d="M0 48h48V0L0 48zm39.2-5.2l-1.6-1.6c-2 3.2-4.4 1.9-4.4 1.9-1.5-.7-2.4-.3-2.4-.3-1.4.3-.5-.6-.5-.6 1-.9 2.4-.5 2.4-.5 2.3.6 3.2-1.2 3.5-2-.5-.7-.7-1.4-.7-1.9-2.3 1.9-5.3 3.2-8.9-.4-1-1-1.5-2.2-1.6-3.5-.3-.1-1.6-.8-2.4-2.6 0 0 .5-1 3.6-1.6.5-1 1.2-1.9 2-2.7.8-.8 1.7-1.5 2.7-2 .6-3 1.6-3.5 1.6-3.5 1.8.8 2.4 2.1 2.6 2.4 1.3.1 2.5.5 3.5 1.6 3.6 3.6 2.2 6.6.3 8.9.6 0 1.5.2 2.4 1.1l2.6 2.6-4.7 4.7z" fill="#fff"/>
