4d7aa1a290f76bf13a27c1d4e606a644f0a1f228
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2019-11-23 18:46:32 +0100

    refactoring

--- src/utilities/getMood.js
+++ src/utilities/getMood.js
@@ -1,14 +1,14 @@
 import { scaleLinear } from "d3-scale";
 
 const valueDomain = [20, 40, 60, 100, 500];
-let colorRange = ['rgba(62, 147, 208, 0.75)',
+let colorRange = ['rgba(45, 121, 185, 0.75)',
   'rgba(249, 168, 37, 0.75)',
   'rgba(230, 81, 0, 0.75)',
   'rgba(221, 44, 0, 0.75)',
   'rgba(150, 0, 132, 0.75)'];
 
 export default function getMood(value, opacity) {
-    colorRange = [`rgba(62, 147, 208, ${opacity})`,
+    colorRange = [`rgba(45, 121, 185, ${opacity})`,
       `rgba(249, 168, 37, ${opacity})`,
       `rgba(230, 81, 0, ${opacity})`,
       `rgba(221, 44, 0, ${opacity})`,
