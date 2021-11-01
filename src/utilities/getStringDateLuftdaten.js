7355fc75927acd356023d9e0770f60af7e76dbdb
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2021-11-01 08:09:02 +0100

    Zeitumstellung

--- src/utilities/getStringDateLuftdaten.js
+++ src/utilities/getStringDateLuftdaten.js
@@ -3,9 +3,12 @@ import getUnixDateFromLuftdaten from './getUnixDateFromLuftdaten';
 
 export default function getStringDateLuftdaten(date) {
     var newDate = getUnixDateFromLuftdaten(date);
-
-    // add 1 hour because of timezone
-    newDate += 7200;
-
+    
+    // Summertime
+    // newDate += 7200;
+  
+    // Wintertime
+    newDate += 3600;
+  
     return getStringDate(newDate * 1000);
 }
